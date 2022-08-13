14881370594a322e8807e85a24131c320260df94
Bethany <morphforlife@gmail.com>
2022-08-13 15:21:31 -0500

    All Exercises complete

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,8 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student bethany = new Student("Bethany", 1, 1, 4.0);
+            Console.WriteLine("The Student class works! " + bethany.Name + " is a student!" + bethany.Name + "'s student ID number is " + bethany.StudentId + ". She has " + bethany.NumberOfCredits + " credit(s) and her GPA is " + bethany.Gpa + "!");
         }
     }
 }
